f8ce3f49a48d2840f7ab96642e3b465ccce55906
rrrwly <155187336+rrrwly@users.noreply.github.com>
2023-12-29 13:16:18 -0500

    Add files via upload

---
+++ anguish/scare.gg/assets/script.js
@@ -0,0 +1,51 @@
+jQuery(document).ready(function ($) {
+
+    $(window).blur(function () {
+        document.title = "jugg me";
+    });
+
+    $(window).focus(function () {
+        document.title = "glo";
+    });
+
+    $('.enter>img').on('click', function () {
+        var audio = new Audio('assets/song.mp3');
+        audio.volume = 0.2;
+        audio.play();
+        $('.enter').fadeOut(100);
+    })
+
+    $('.login').click(function () {
+        window.location.href = '/login';
+    });
+
+    $('.roster').click(function () {
+        $('.wrap').fadeOut(100)
+        $('.roster-wrap').fadeIn(250);
+    });
+
+    $('.login').on({
+
+        mouseenter: function () {
+            $(this).find('.overlay').css('background', 'rgba(0,0,0,0)').fadeIn(100)
+        },
+
+        mouseleave: function () {
+            $(this).find('.overlay').css('background', 'rgba(0,0,0,.5)').fadeIn(100)
+        }
+
+    })
+
+    $('.roster').on({
+
+        mouseenter: function () {
+            $(this).find('.overlay').css('background', 'rgba(0,0,0,0)').fadeIn(100)
+        },
+
+        mouseleave: function () {
+            $(this).find('.overlay').css('background', 'rgba(0,0,0,.5)').fadeIn(100)
+        }
+
+    })
+
+});
