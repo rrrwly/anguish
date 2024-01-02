757b37468c4d79b5a0c916cd55b632e265c06b1c
rrrwly <155187336+rrrwly@users.noreply.github.com>
2024-01-01 21:06:21 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -13,23 +13,26 @@ jQuery(document).ready(function ($) {
         audio.volume = 0.3;
         audio.play();
         $('.enter').fadeOut(100);
-    })
+    });
+
+    $('.clique').on('click', function () {
+        var audio = new Audio('assets/song.mp3');
+        audio.volume = 0.3;
+        audio.play();
+    });
 
     $('.roster').click(function () {
-        $('.wrap').fadeOut(100)
+        $('.wrap').fadeOut(100);
         $('.roster-wrap').fadeIn(250);
     });
 
     $('.roster').on({
-
         mouseenter: function () {
-            $(this).find('.overlay').css('background', 'rgba(0,0,0,0)').fadeIn(100)
+            $(this).find('.overlay').css('background', 'rgba(0,0,0,0)').fadeIn(100);
         },
-
         mouseleave: function () {
-            $(this).find('.overlay').css('background', 'rgba(0,0,0,.5)').fadeIn(100)
+            $(this).find('.overlay').css('background', 'rgba(0,0,0,.5)').fadeIn(100);
         }
-
-    })
+    });
 
 });
